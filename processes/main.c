caf0ee892dd974d7b1642564e97b6d26051d94cc
tunashred <enachealexandru.traian@gmail.com>
2024-06-29 13:54:25 +0300

    Simple pipe between processes

--- processes/main.c
+++ processes/main.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
+// for pipes:
+// write - 0
+// read  - 1
 int main() {
     pid_t pid;
-    char *args_ls[] = {"ls", "-la", "/", 0};
-    char *env_ls[] = {0};
+    int descriptor[2];
+    int data;
+    char message[69] = "Azi mananc peste proaspat";
+    char buffer[420];
 
-    if((pid = fork()) < 0) {
-        fprintf(stderr, "Unable to fork a new process\n");
+    pipe(descriptor);
+
+    if( (pid = fork()) < 0) {
+        fprintf(stderr, "Unable to create new process\n");
+        exit(1);
     }
-    
+
     if(pid == 0) {
-        execve("/bin/ls", args_ls, env_ls);
+        close(descriptor[1]);
+        data = read(descriptor[0], buffer, sizeof(buffer));
+        printf("Printing data value: %d\n", data);
+        printf("Message received from parent: %s\n", buffer);
     }
     else {
-        printf("420\n");
+        close(descriptor[0]);
+        sleep(2);
+        write(descriptor[1], message, strlen(message) + 1);
+        wait(0);
     }
-    
 
     return 0;
 }
