a0dcebcb8ce59a818bfb086606f00183ff30c70a
tunashred <enachealexandru.traian@gmail.com>
2024-07-04 23:19:20 +0300

    Simple mutex usage example

--- threads/pthreads/main.c
+++ threads/pthreads/main.c
@@ -1,26 +1,35 @@
+#include <pthread.h>
+#include <bits/pthreadtypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
-void* print_message(void *ptr) {
-    char* message = (char*) ptr;
-    printf("%s\n", message);
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* increment(void* x) {
+    pthread_mutex_lock(&mutex);
+    int* x_int = (int*) x;
+    for(int i = 0; i < 1000; i++) {
+        (*x_int)++;
+    }
+    pthread_mutex_unlock(&mutex);
+    
+    return NULL;
 }
 
 int main() {
-    pthread_t thread1, thread2;
-    char* message1 = "Thread 1";
-    char* message2 = "Thread 2";
-    int iret1, iret2;
+    int x = 0;
+    pthread_t thread1, thread2, thread3;
 
-    iret1 = pthread_create(&thread1, NULL, print_message, message1);
-    iret2 = pthread_create(&thread2, NULL, print_message, message2);
-
+    pthread_create(&thread1, NULL, increment, &x);
+    pthread_create(&thread2, NULL, increment, &x);
+    pthread_create(&thread3, NULL, increment, &x);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
 
-    printf("Thread 1 returns: %d\n", iret1);
-    printf("Thread 2 returns: %d\n", iret2);
+    printf("%d\n", x);
 
+    pthread_mutex_destroy(&mutex);
+    
     return 0;
 }
