3661096b2ee1ea99d980ae28cb2ac075938b7b65
tunashred <enachealexandru.traian@gmail.com>
2024-08-18 19:39:11 +0300

    Read-write lock example with writer preference

--- threads/pthreads/main.c
+++ threads/pthreads/main.c
@@ -9,31 +9,38 @@
 #include <semaphore.h>
 #include <errno.h>
 
-#define MAX_CONCURRENT_THREADS 2
-sem_t semaphore;
-
 char* buffer[] = {"john", "carmack", "jonathan", "blow", "jim", "keller"};
+pthread_rwlock_t rw_lock;
 
 void* read_buffer() {
-    sem_wait(&semaphore);
-    printf("%s %s\n", buffer[0], buffer[1]);
-    sleep(2);
-    sem_post(&semaphore);
+    pthread_rwlock_rdlock(&rw_lock);
+    printf("%s\n", buffer[0]);
+    pthread_rwlock_unlock(&rw_lock);
+
+    // sleep(1);
+
+    pthread_rwlock_rdlock(&rw_lock);
+    printf("%s\n", buffer[0]);
+    pthread_rwlock_unlock(&rw_lock);
     return NULL;
 }
 
 void* write_buffer() {
-    sem_wait(&semaphore);
-    buffer[0] = "69\0";
-    sem_post(&semaphore);
+    pthread_rwlock_wrlock(&rw_lock);
+    buffer[0] = "69";
+    pthread_rwlock_unlock(&rw_lock);
     return NULL;
 }
 
 int main() {
-    sem_init(&semaphore, 0, MAX_CONCURRENT_THREADS);
     pthread_t reader1, reader2, writer;
+    pthread_rwlockattr_t rwlock_attr;
+    pthread_rwlockattr_init(&rwlock_attr);
+    pthread_rwlockattr_setkind_np(&rwlock_attr, PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
+
+    pthread_rwlock_init(&rw_lock, &rwlock_attr);
+    pthread_rwlockattr_destroy(&rwlock_attr);
     
-    //TODO: lookup pthread_attr_t
     pthread_create(&reader1, NULL, read_buffer, NULL);
     pthread_create(&reader2, NULL, read_buffer, NULL);
     pthread_create(&writer, NULL, write_buffer, NULL);
@@ -42,7 +49,7 @@ int main() {
     pthread_join(reader2, NULL);
     pthread_join(writer, NULL);
 
-    printf("%s\n", buffer[0]);
+    pthread_rwlock_destroy(&rw_lock);
 
     return 0;
 }
